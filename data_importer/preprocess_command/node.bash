58335ea82f45735fa721f5a3156cc45062275d8f
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2022-05-03 10:01:07 +0900

    Edit wrapper shell scripts to rename the docker container

--- data_importer/preprocess_command/node.bash
+++ data_importer/preprocess_command/node.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -v $(pwd)/config:/opt/config -v $(pwd)/data_files:/opt/data_files test:0.0.1 -c /opt/config/fiware_config.json -t node $1
+docker run -it -v $(pwd)/config:/opt/config -v $(pwd)/data_files:/opt/data_files data_importer:0.1.0 -c /opt/config/fiware_config.json -t node $1
